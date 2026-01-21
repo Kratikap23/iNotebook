a873f9984d26d21a850eaeb9e594fb6c8fb81e2e
Kratikap23 <kratikap665@gmail.com>
2026-01-21 21:56:14 +0530

    Adding Signup Component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,10 +15,11 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+    let success = false;
     //If there are errors , return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({success, errors: errors.array() });
     }
 
     // Check whether the user with this email exists already
@@ -26,7 +27,7 @@ router.post('/createuser', [
         let user = await User.findOne({ email: req.body.email });
 
         if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+            return res.status(400).json({success, error: "Sorry a user with this email already exists" })
         }
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
@@ -52,7 +53,8 @@ router.post('/createuser', [
 
 
         //res.json(user);
-        res.json({ authtoken })
+        success=true;
+        res.json({ success,authtoken })
 
     }
     catch (error) {
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Login from './components/Login';
-import Signup from './components/signup';
+import Signup from './components/Signup';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Alert from './components/Alert';
